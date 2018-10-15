78b46374a9172bbeb0abf5da3fe32be7fb737958
Ayesha W <ayesha.wan@youdo.co.nz>
2018-10-16 10:43:26 +1300

    Init commit

---
+++ components-udemy/webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+	entry: "./app/js/app.js",
+	output: {
+		path: __dirname + '/public',
+		filename: 'bundle.js'
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.jsx?$/,
+				exclude: /node_modules/,
+				loader: 'babel',
+				query: {
+					presets: ['react', 'es2015']
+				}
+			}
+		]
+	}
+}
