a91c34434aa69649a254021918d7c388aa1cb9b2
Ayesha W <ayesha.wan@youdo.co.nz>
2018-10-17 16:43:41 +1300

    Splits component into sub component

--- main-components/app/js/components/Timer.js
+++ main-components/app/js/components/Timer.js
@@ -9,15 +9,23 @@ export default class Timer extends React.Component {
     this.state = {
       time: 0
     }
-    this.handleClick = this.handleClick.bind(this)
+    this.handleClick = this.handleClick. bind(this)
+  }
+
+  componentDidMount() {
+    this.timer = setInterval(
+      () => this.startTimer(),
+      1000)
+  }
+
+  startTimer() {
+    this.setState(prevState => ({
+      time: prevState.time += 1
+    }))
   }
 
   handleClick() {
-    setInterval(() => {
-      this.setState(prevState => ({
-        time: prevState.time += 1
-      }))
-    }, 1000)
+    clearInterval(this.timer)
   }
 
   render() {
