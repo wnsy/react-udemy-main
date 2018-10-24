48801f3a1dd087c5efa2ae3743ea7bfe13091b03
Ayesha W <ayesha.wnz@gmail.com>
2018-10-25 12:02:58 +1300

    Uses events to influence app display

--- states-events/app/js/components/Timer/NameList.js
+++ states-events/app/js/components/Timer/NameList.js
@@ -19,31 +19,28 @@ export default class NameList extends React.Component {
 
   handleClick(event) {
     event.preventDefault();
-    this.setState({
-      event: event.type
-    })
   }
 
   nameFilter(event) {
-    let newNames = this.state.names.filter((name) => {
-      let fullName = `${name.first_name} ${name.last_name}`
-      return fullName.toLowerCase().includes(event.target.value.toLowerCase());
-    });
     //capture the input
     this.setState({
-      names: newNames
+      filterText: event.target.value
     })
   }
 
   render() {
     let {names} = this.state; //ES6 way. but same as var names = this.state.names
+    let {filterText} = this.state;
+    if (filterText) {
+      names = names.filter((name) => {
+        let fullName = `${name.first_name} ${name.last_name}`;
+        return fullName.toLowerCase().includes(filterText.toLowerCase());
+      });
+    }
     return (
       <div>
         <h2>{this.state.event}</h2>
-        <input
-          onChange={this.nameFilter}
-          type="text"
-        />
+        <input onChange={this.nameFilter} type="text" />
         {names.map((name) => <h3 key={name.id}><a href="#" onClick={this.handleClick}>{name.first_name}</a></h3>)}
       </div>
     )
